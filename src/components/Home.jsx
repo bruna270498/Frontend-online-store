bfc69cb85cc7b9cc818be042a658487800980d75
Bruna <brunaalmeida075@gmail.com>
2022-11-09 17:05:04 -0300

    requisito 4 feito

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 export default class Home extends Component {
+  state = {
+    categoria: [],
+  };
+
+  async componentDidMount() {
+    const categoria = await getCategories();
+    this.setState({
+      categoria,
+    });
+  }
+
   render() {
+    const { categoria } = this.state;
     return (
       <div>
         <h1
@@ -12,6 +25,20 @@ export default class Home extends Component {
 
         </h1>
         <Link to="/carrinho" data-testid="shopping-cart-button">Carrinho de Compras</Link>
+        <div>
+          {categoria.map((e) => (
+            <label key={ e.id } htmlFor={ e.name } data-testid="category">
+              {e.name}
+              <input
+                type="radio"
+                value={ e.name }
+                id={ e.name }
+                name="category"
+                onClick=""
+              />
+            </label>
+          ))}
+        </div>
       </div>
     );
   }
