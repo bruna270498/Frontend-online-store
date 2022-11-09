08d186a5053341416104fd6b9901f5c789bb6de7
Bruna <brunaalmeida075@gmail.com>
2022-11-09 16:43:14 -0300

    resolução requisito 3

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class Home extends Component {
           Digite algum termo de pesquisa ou escolha uma categoria.
 
         </h1>
+        <Link to="/carrinho" data-testid="shopping-cart-button">Carrinho de Compras</Link>
       </div>
     );
   }
