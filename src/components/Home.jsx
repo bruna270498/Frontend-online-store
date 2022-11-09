271ac4d399aa60a0ab08eea70e0707de56c2831f
Reivan Chagas <chagasreivan@gmail.com>
2022-11-09 16:22:12 -0300

    Adiciona resolução req2 dnv dnv

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,12 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <h2
+        <h1
           data-testid="home-initial-message"
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
 
-        </h2>
+        </h1>
       </div>
     );
   }
