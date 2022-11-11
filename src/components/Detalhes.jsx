9650663f1a6232b155a16e9ebca7dac4b81547d7
Bruna <brunaalmeida075@gmail.com>
2022-11-11 16:41:28 -0300

    requisito 11 quase terminado

--- src/components/Detalhes.jsx
+++ src/components/Detalhes.jsx
@@ -6,6 +6,12 @@ import { getProductById } from '../services/api';
 export default class Detalhes extends Component {
   state = {
     produto: '',
+    email: '',
+    avalia: '',
+    comentario: '',
+    mensagemError: false,
+    infoForm: [],
+    comentarioAvaliador: [],
   };
 
   async componentDidMount() {
@@ -14,6 +20,8 @@ export default class Detalhes extends Component {
     this.setState({
       produto,
     });
+    const comentarioAvaliador = JSON.parse(localStorage.getItem(produto.id));
+    this.setState({ comentarioAvaliador });
   }
 
   adicionarProdutoaoCarrinho = (produto) => {
@@ -44,8 +52,37 @@ export default class Detalhes extends Component {
     }
   };
 
+  salvarEstado = ({ target }) => {
+    const { name } = target;
+    const value = (target.checkbox) ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  salvarForm = () => {
+    const { comentario, email, avalia, infoForm } = this.state;
+    const informacao = { email, avalia, comentario };
+    this.setState({
+      infoForm: [...infoForm, informacao],
+    });
+  };
+
+  avaliarBtn = (event) => {
+    event.preventDefault();
+    const { avalia, email, infoForm, produto } = this.state;
+    if (avalia === '' || email === '') {
+      // retorna a mensagem de erro campos invalidos
+      this.setState({
+        mensagemError: true,
+      });
+    } else { this.setState({ mensagemError: false }); }
+    this.salvarForm();
+    localStorage.setItem(produto.id, JSON.stringify(infoForm));
+  };
+
   render() {
-    const { produto } = this.state;
+    const { produto, mensagemError, comentarioAvaliador } = this.state;
     return (
       <div>
         <h1>Detalhes do Produto</h1>
@@ -78,6 +115,83 @@ export default class Detalhes extends Component {
         >
           vá para o carrinho
         </Link>
+        <form>
+          <input
+            data-testid="product-detail-email"
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={ this.salvarEstado }
+            required
+          />
+          <input
+            onChange={ this.salvarEstado }
+            data-testid="1-rating"
+            type="radio"
+            name="avalia"
+            value="1"
+            required
+          />
+          <input
+            onChange={ this.salvarEstado }
+            data-testid="2-rating"
+            type="radio"
+            name="avalia"
+            value="2"
+            required
+          />
+          <input
+            onChange={ this.salvarEstado }
+            data-testid="3-rating"
+            type="radio"
+            name="avalia"
+            value="3"
+            required
+          />
+          <input
+            onChange={ this.salvarEstado }
+            data-testid="4-rating"
+            type="radio"
+            name="avalia"
+            value="4"
+            required
+          />
+          <input
+            onChange={ this.salvarEstado }
+            data-testid="5-rating"
+            type="radio"
+            name="avalia"
+            value="5"
+            required
+          />
+          <textarea
+            data-testid="product-detail-evaluation"
+            onChange={ this.salvarEstado }
+            type="text"
+            name="comentario"
+            placeholder="Deixe seu Comentário"
+          />
+          <button
+            type="submit"
+            data-testid="submit-review-btn"
+            onClick={ this.avaliarBtn }
+          >
+            Avaliar
+
+          </button>
+          {mensagemError && (
+
+            <span data-testid="error-msg">Campos inválidos</span>
+
+          )}
+        </form>
+        {/* {comentarioAvaliador.map((e, index) => (
+          <div key={ index }>
+            <h3>{e.email}</h3>
+            <h3>{e.avalia}</h3>
+            <p>{e.comentario}</p>
+          </div>
+        ))} */}
       </div>
     );
   }
